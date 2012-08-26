8d8b995f676f6925541339480dc12624e8ce6316
Noah Glusenkamp <nglusenkamp@IWK010960-064.internal.ngpvan.net>
2012-08-26 15:20:13 -0700

    Changed grid data model from Matrix to Array

--- assignment_1/Percolation.java
+++ assignment_1/Percolation.java
@@ -1,12 +1,25 @@
 public class Percolation {
 
     private int N;
-    private int[][] grid;
+    private int[] grid;
+    WeightedQuickUnionUF UF;
 
     public Percolation(int N) {
     //create an N-by-N grid with all sites blocked
         this.N = N;
-        grid = new int[N][N];
+        grid = new int[N*N];
+        
+        //every cell plus virual top site and virtual bottom site
+        UF = new WeightedQuickUnionUF((N*N)+2);
+        
+        //union each top row to virtual top site
+        
+    }
+    
+    private int coordinatesToPosition(int i, int j){
+    //returns the Array positon for grid coordinates orignating 1,1 upper left
+        checkInBounds(i, j);
+        return N*i-(N-j+1);
     }
     
     private void checkInBounds(int i, int j) {
@@ -19,7 +32,7 @@ public class Percolation {
     public boolean isOpen(int i, int j) {    
     // is site (row i, column j) open?        
         checkInBounds(i, j);
-        if (this.grid[i][j] != 0) { return true; } 
+        if (grid[coordinatesToPosition(i, j)] != 0) { return true; } 
         return false; 
 
     } 
@@ -27,7 +40,7 @@ public class Percolation {
     public boolean isFull(int i, int j) {    
     // is site (row i, column j) full?
         checkInBounds(i, j);
-        if (this.grid[i][j] == 0) { return true; }
+        if (grid[coordinatesToPosition(i, j)] == 0) { return true; }
         return false;
     
     }
@@ -35,16 +48,14 @@ public class Percolation {
     public void open(int i, int j) {
     //open a site if it is not already
         checkInBounds(i, j);
-        this.grid[i][j] = 1;
+        this.grid[coordinatesToPosition(i, j)] = 1;
         
     }
     
     public boolean percolates() {
     //does the system percolate?
         
-        //initialize a unionfind object of size (N^2)+2 
-        //every cell plus virual top site and virtual bottom site
-        WeightedQuickUnionUF UF = new WeightedQuickUnionUF((N*N)+2);
+        //WeightedQuickUnionUF UF = new WeightedQuickUnionUF((N*N)+2);
         System.out.println(UF.count());
         
         return true;
@@ -53,15 +64,16 @@ public class Percolation {
     public static void main(String[] args) {
     //test client
      
-     Percolation perc = new Percolation(100);
-     System.out.println(perc.isFull(50, 50));
-     System.out.println(perc.isOpen(50, 50));
-     perc.open(50, 50);
-     System.out.println(perc.isFull(50, 50));
-     System.out.println(perc.isOpen(50, 50));
-     System.out.println(perc.grid[0].length);
-
-     perc.percolates();
+        Percolation perc = new Percolation(100);
+        System.out.println(perc.isFull(50, 50));
+        System.out.println(perc.isOpen(50, 50));
+        perc.open(50, 50);
+        System.out.println(perc.isFull(50, 50));
+        System.out.println(perc.isOpen(50, 50));
+        System.out.println(perc.coordinatesToPosition(50, 50));
+        System.out.println(perc.coordinatesToPosition(1, 1));
+        System.out.println(perc.coordinatesToPosition(2, 1));
+        perc.percolates();
     
     }
     
