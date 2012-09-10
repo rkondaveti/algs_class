ae0f6afd22c36f5b0642a5a8b195135709f40df4
Noah Glusenkamp <me@noahglusenkamp.com>
2012-09-10 13:10:25 -0700

    Playing with using and object to hold confidence interval.

--- src/LearnStats.java
+++ src/LearnStats.java
@@ -6,6 +6,7 @@
 public class LearnStats {
     
     double[] randomNumbers;
+    ConfidenceInterval confidenceInterval;
     
     public LearnStats(int size) {
     //create an array of requested size, fill it with random numbers
@@ -15,6 +16,9 @@ public class LearnStats {
         for (int i = 0; i < size; i++) {
             randomNumbers[i] = StdRandom.random();
         }
+        
+        confidenceInterval = new ConfidenceInterval(randomNumbers);
+        
     }
     
     public void printValues() {
@@ -25,14 +29,41 @@ public class LearnStats {
     
     }
     
+    final class ConfidenceInterval{
+        private final double left;
+        private final double right;
+        
+        public ConfidenceInterval(double[] inputArray) {
+            
+            double mean = StdStats.mean(inputArray);
+            double stddev = StdStats.stddev(inputArray);
+        
+            this.left = mean - 1.96 * stddev / Math.sqrt(inputArray.length);
+            this.right = mean + 1.96 * stddev / Math.sqrt(inputArray.length);
+        }
+        
+        public double getLeft(){
+            return left;
+        }
+        
+        public double getRight(){
+            return right;
+        }
+    }
+    
     public static void main(String[] args) {
         
-        LearnStats tester = new LearnStats(10);
+        LearnStats tester = new LearnStats(100);
         tester.printValues();
         
-        System.out.println("Mean: " + StdStats.mean(tester.randomNumbers));
-        System.out.println("Standard Deviation: " + StdStats.stddev(tester.randomNumbers));
-        
+        System.out.println("Mean: " + 
+                           StdStats.mean(tester.randomNumbers));
+        System.out.println("Standard Deviation: " + 
+                           StdStats.stddev(tester.randomNumbers));
+        System.out.println("95% Confidence Interval: " + 
+                           tester.confidenceInterval.getLeft() + ", " +
+                           tester.confidenceInterval.getRight()
+                          );
     }   
 
 }
